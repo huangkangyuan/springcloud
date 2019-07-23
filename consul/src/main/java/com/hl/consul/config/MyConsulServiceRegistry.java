13dc976e273b9bea86b17cc79a3ad2d7d7996c7b
huangkangyuan <614772276@qq.com>
2019-07-23 14:50:36 +0800

    consul注册

---
+++ consul/src/main/java/com/hl/consul/config/MyConsulServiceRegistry.java
@@ -0,0 +1,21 @@
+package com.hl.consul.config;
+
+import com.ecwid.consul.v1.ConsulClient;
+import org.springframework.cloud.consul.discovery.ConsulDiscoveryProperties;
+import org.springframework.cloud.consul.discovery.HeartbeatProperties;
+import org.springframework.cloud.consul.discovery.TtlScheduler;
+import org.springframework.cloud.consul.serviceregistry.ConsulRegistration;
+import org.springframework.cloud.consul.serviceregistry.ConsulServiceRegistry;
+
+public class MyConsulServiceRegistry extends ConsulServiceRegistry {
+
+    public MyConsulServiceRegistry(ConsulClient client, ConsulDiscoveryProperties properties, TtlScheduler ttlScheduler, HeartbeatProperties heartbeatProperties) {
+        super(client, properties, ttlScheduler, heartbeatProperties);
+    }
+
+    @Override
+    public void register(ConsulRegistration reg) {
+        reg.getService().setId(reg.getService().getName() + "-" + reg.getService().getAddress() + "-" + reg.getService().getPort());
+        super.register(reg);
+    }
+}
---
+++ consul/src/main/java/com/hl/consul/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.hl.consul.controller;
+
+import com.hl.consul.domain.User;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    @PostMapping(value = "/user",
+            consumes = MediaType.APPLICATION_XML_VALUE,
+            produces = MediaType.APPLICATION_XML_VALUE)
+    @ResponseBody
+    public User create(@RequestBody User user) {
+        user.setName("huangkangyuan.com : " + user.getName());
+        user.setAge(user.getAge() + 100);
+        return user;
+    }
+}
